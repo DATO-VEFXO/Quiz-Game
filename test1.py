f64f872752f6520d8f4544a5a791f556febbacc8
DATO-VEFXO <107356352+DATO-VEFXO@users.noreply.github.com>
2026-01-14 16:25:44 +0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,17 +1,13 @@
-from ascii_art import show_welcome
-from quiz_logic import start_quiz
-
 def main():
     show_welcome()
     while True:
-        choice = input("გსურთ თამაში დაიწყოთ? (Y/N): ").upper()
-        if choice == "Y":
+        choice = input("გსურთ თამაში დაიწყოთ? (KI/ARA): ").upper()
+        if choice == "KI":
             start_quiz()
-        elif choice == "N":
+        elif choice == "ARA":
             print("თამაში დასრულდა. ნახვამდის!")
             break
         else:
-            print("გთხოვთ შეიყვანოთ მხოლოდ Y ან N.\n")
-
+            print("გთხოვთ შეიყვანოთ მხოლოდ KI ან ARA.\n")
 if __name__ == "__main__":
     main()
